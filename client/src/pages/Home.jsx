c1b2fcfa1fa78c31f56719a5773c5e2fc9de4414
Ritik <ritik2662004@gmail.com>
2026-07-07 23:16:35 +0530

    First commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+function Home() {
+    return (
+        <div>
+            <h1>AI Investment Research Agent</h1>
+
+            <p>
+                Research any company and get an AI-powered
+                investment recommendation.
+            </p>
+        </div>
+    );
+}
+
+export default Home;
